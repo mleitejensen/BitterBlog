7e151e22aceca9c7c663aad8fa299865e43c78cf
Martin Leite Jensen <marekatt2004@gmail.com>
2024-04-03 20:33:44 +0200

    Added authContext to frontend

--- Bitter/src/main.jsx
+++ Bitter/src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthContextProvider } from './context/authContext.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
 )
