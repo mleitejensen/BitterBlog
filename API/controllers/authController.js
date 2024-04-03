914e38dcd279dd90cfe444a844d9cd3a3952fab1
Martin <marekatt2004@gmail.com>
2024-04-03 10:56:45 +0200

    added monkey code

---
+++ API/controllers/authController.js
@@ -0,0 +1,14 @@
+const signup = (req, res) => {
+
+    res.stastu(202).json({message: "hello"})
+}
+
+const login = (req, res) => {
+
+    res.stastu(202).json({message: "hello"})
+}
+
+module.exports = {
+    signup,
+    login
+}
---
+++ API/controllers/dataController.js
@@ -0,0 +1,8 @@
+const getLatest = (req, res) => {
+
+    res.stastu(202).json({message: "hello"})
+}
+
+module.exports = {
+    getLatest,
+}
