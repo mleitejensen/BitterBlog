02e6fe3b5e0022a32c433a3912a4f00a04dd3ca9
Martin Leite Jensen <marekatt2004@gmail.com>
2024-04-03 22:00:01 +0200

    Now checks local storage for a jwt to login

--- Bitter/src/components/Navbar.jsx
+++ Bitter/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import '../App.css'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
   const {logout} = useLogout()
+  const {user} = useAuthContext()
 
   const handleClick = () => {
     logout()
@@ -17,17 +19,22 @@ const Navbar = () => {
           <h1>Bitter</h1>
         </Link>
 
-        <div>
-          <button onClick={handleClick}>Logout</button>
-        </div>
-        <div>
-          <Link to="/login">
-              Login
+        {user && (
+          <div>
+            <span>{user.username}</span>
+            <button onClick={handleClick}>Logout</button>
+          </div>
+        )}
+        {!user && (
+          <div>
+            <Link to="/login">
+                Login
+            </Link>
+            <Link to="/signup">
+                Signup
           </Link>
-          <Link to="/signup">
-              Signup
-        </Link>
-        </div>
+          </div>
+        )}
         </nav>
     </header>
   )
