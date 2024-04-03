237cdc2bb7ca369a29f291daaf8cc840f99584e5
Martin <marekatt2004@gmail.com>
2024-04-03 12:45:16 +0200

    /data now only gets the 5 latests post

--- API/controllers/dataController.js
+++ API/controllers/dataController.js
@@ -1,11 +1,16 @@
 const DataModel = require("../models/dataModel")
 
 const maxBodyLength = 100
+const latestPosts = 5
 
 const getLatest = async (req, res) => {
     try{
-        const dataList = await DataModel.find({})
-        res.status(202).json(dataList)
+        let latestList = []
+        const dataList = await DataModel.find({}).sort({ createdAt: 'desc'}).exec();
+        for(let i = 0; i < latestPosts; i++){
+            latestList.push(dataList[i])
+        }
+        res.status(202).json(latestList)
     } catch(error){
         res.status(400).json({ error: error.message })
     }
