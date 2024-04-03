7dc93d54791199a2e057ea237bbac2f9f1a0ecfe
Martin <marekatt2004@gmail.com>
2024-04-03 11:21:02 +0200

    Added signup and login

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,11 +1,34 @@
-const signup = (req, res) => {
+const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
 
-    res.stasus(202).json({message: "hello"})
+const createWebToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1d' })
 }
 
-const login = (req, res) => {
+const signup = async (req, res) => {
+    const { username, password } = req.body;
 
-    res.stasus(202).json({message: "hello"})
+    try{
+        const user = await User.signup(username, password)
+        res.status(200).json({ result: `${user.username} created`})
+    }catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try{
+      const user = await User.login(username, password)
+      if(user.admin === false){
+        throw Error("You are not an admin")
+      }
+      const token = createWebToken(user._id)
+      res.status(200).json({username, token})
+    }catch(error){
+      res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = {
