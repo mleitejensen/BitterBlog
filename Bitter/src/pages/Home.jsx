e832f4eb54e12544317b2a7eac0219416565cf9d
Martin <marekatt2004@gmail.com>
2024-04-04 10:11:23 +0200

    Now gets all posts from a user on their home page

--- Bitter/src/pages/Home.jsx
+++ Bitter/src/pages/Home.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Home = () => {
-
     const { user } = useParams();
-    console.log(user);
+    const [posts, setPosts] = useState(null)
+    const [error, setError] = useState(null)
+
+    const makeApiCall = async () => {
+        const response = await fetch(`http://localhost:3000/user/${user}`)
+        const json = await response.json()
+
+        if(response.ok){
+            setPosts(json)
+        }
+        if(!response.ok){
+            setError(json.error)
+        }
+    }
+
+    useEffect(() => {
+        makeApiCall()
+    }, [user])
 
 
     return(
         <>
-            <div>Home</div>
+        {posts && posts.map((post) => (
+            <div className="post" key={post._id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+
+        ))}
+
+        {error && (
+            <div className="error">{error}</div>
+        )}
         </>
     )
 }
