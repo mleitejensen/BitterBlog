055b51cfaf350efb29b23cc20217de6b9b3d59ac
Martin <marekatt2004@gmail.com>
2024-04-04 11:05:15 +0200

    Made a usePost hook to publish to Bitter

--- Bitter/src/pages/Home.jsx
+++ Bitter/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { usePost } from "../hooks/usePost"
 
 const Home = () => {
     const { user } = useParams();
     const [posts, setPosts] = useState(null)
-    const [error, setError] = useState(null)
+    //const [error, setError] = useState(null)
+
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+    const {post, error, isLoading, data} = usePost()
 
     const makeApiCall = async () => {
         const response = await fetch(`http://localhost:3000/user/${user}`)
@@ -22,9 +27,36 @@ const Home = () => {
         makeApiCall()
     }, [user])
 
+    useEffect(() => {
+        makeApiCall()
+    },[data])
+
+    const publish = async (e) => {
+        e.preventDefault()
+
+        await post(title, body, user)
+    }
 
     return(
         <>
+
+        <form className="publish">
+            <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <div></div>
+            <textarea 
+                cols={45} 
+                rows={4} 
+                maxLength="100" 
+                placeholder="Content here..." 
+                reset="true" 
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+            {error && <div className="error">{error}</div>}
+            <button disabled={isLoading} onClick={publish}>Publish</button>
+
+        </form>
+
         {posts && posts.map((post) => (
             <div className="post" key={post._id}>
                 <h3>{post.title}</h3>
