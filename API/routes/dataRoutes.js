2061e5397e393aa25de445bcac77e7d7c00d598a
Martin Leite Jensen <marekatt2004@gmail.com>
2024-04-06 00:55:39 +0200

    Added token atuorization to the APO '/post' route

--- API/routes/dataRoutes.js
+++ API/routes/dataRoutes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const router = Router()
 const { getLatest, createPost, getUserPosts } = require("../controllers/dataController")
+const {requireAuth} = require("../middleware/requireAuth")
+
 
 router.get("/latest", getLatest)
-router.post("/post", createPost)
 router.get("/user/:user", getUserPosts)
+
+router.use(requireAuth)
+router.post("/post", createPost)
 
 module.exports = router
